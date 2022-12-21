94abb3c297aed6a9dbd97461a73fa406890ee62e
Daniel De Pascale <depascale.daniel.92@gmail.com>
2022-12-21 16:47:28 +0100

    Changed data dest structure.

--- port_tracking.py
+++ port_tracking.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     if not os.path.exists(data_folder):
         os.mkdir(data_folder)
 
-    today_folder = os.path.join(data_folder, datetime.today().strftime('%Y-%m-%d'))
+    today_folder = os.path.join(data_folder, datetime.today().strftime('%Y-%m-%d %Hh%mm'))
     today_tms = int(datetime.today().timestamp())
 
     ports = crawler.search_port(port)
@@ -45,20 +45,32 @@ if __name__ == '__main__':
     if not os.path.exists(today_folder):
         os.mkdir(today_folder)
 
+    # Create port folder
+    i = 0
+    while i < len(ports) and ports[i]['id'] != str(port_id):
+        i += 1
+
+    if i == len(ports):
+        print(f"No port found under id {port_id}")
+        sys.exit(0)
+
+    port_folder = os.path.join(today_folder, ports[i]['name'])
+    os.mkdir(port_folder)
+
     print("\nINPORT extraction")
     inport_vessels = crawler.get_inport_vessels(port_id)
     inport_filename = f"port_{port_id}_{today_tms}_inport.csv"
-    save_csv(inport_vessels, os.path.join(today_folder, inport_filename))
+    save_csv(inport_vessels, os.path.join(port_folder, inport_filename))
 
     print("\nArrivals extraction")
     arrival_vessels = crawler.get_arrivals(port_id)
     arrival_filename = f"port_{port_id}_{today_tms}_arrivals.csv"
-    save_csv(arrival_vessels, os.path.join(today_folder, arrival_filename))
+    save_csv(arrival_vessels, os.path.join(port_folder, arrival_filename))
 
     print("\nINPORT calls extraction")
     inport_calls = crawler.get_inport_calls(port_id)
     calls_filename = f"port_{port_id}_{today_tms}_calls.csv"
-    save_csv(inport_calls, os.path.join(today_folder, calls_filename))
+    save_csv(inport_calls, os.path.join(port_folder, calls_filename))
 
     print(f"\nVESSEL analysis. Total vessels: {len(arrival_vessels)}")
     i = 1
@@ -68,8 +80,8 @@ if __name__ == '__main__':
 
         ports = crawler.get_vessel_last_ports(vessel['mmsi'])
         port_filename = f"vessel_{vessel['mmsi']}_{today_tms}_ports.csv"
-        save_csv(ports, os.path.join(today_folder, port_filename))
+        save_csv(ports, os.path.join(port_folder, port_filename))
 
         events = crawler.get_vessel_events(vessel['mmsi'])
         event_filename = f"vessel_{vessel['mmsi']}_{today_tms}_events.csv"
-        save_csv(events, os.path.join(today_folder, event_filename))
+        save_csv(events, os.path.join(port_folder, event_filename))
